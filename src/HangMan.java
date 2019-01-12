82c8c392857a57c388a6ececa1f200a92b871dcc
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-01-12 11:21:52 +0530

    added new BufferReader

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,6 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HangMan {
     public static void main(String[] args) {
-        System.out.println("Hello World !!");
-        //this is me making changes
+
+        System.out.println("Enter The Secret Word");
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringBuffer word;
+
+        try {
+            word = new StringBuffer(br.readLine());
+            System.out.println("Your Word is :" +word);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
