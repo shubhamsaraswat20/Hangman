e521ba48585ffaf6945f4c95a4ad8375d4216887
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-01-30 10:45:07 +0530

    init commit

---
+++ src/CanteenCustomer.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class CanteenCustomer {
+
+    public static void main(String[] args) {
+
+        // primitive data types are:-
+        // int, float, double, char, long,
+        // short, byte, boolean
+
+        float[] arr = new float[10];
+        System.out.println(arr[0]);
+
+        String[] str = new String[10];
+        System.out.println(str[0]);
+
+        Object[] obj = new Object[10];
+        System.out.println(obj[0]);
+
+    }
+
+}
