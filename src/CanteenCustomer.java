55295da26b28c3d0589a4fa16d4dbd03148a3439
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-01-30 11:23:43 +0530

    new Student array

--- src/CanteenCustomer.java
+++ src/CanteenCustomer.java
@@ -17,6 +17,37 @@ public class CanteenCustomer {
         Object[] obj = new Object[10];
         System.out.println(obj[0]);
 
+        Student student = new Student();
+        student.name = "Shubh";
+        student.rollNo = 42;
+
+        Student[] students = new Student[47];
+
+        for (int i = 0; i < students.length; i++) {
+
+            students[i] = new Student();
+            students[i].rollNo = i+1;
+            students[i].name = "Shubh" + i;
+
+        }
+
+        for (Student student1 : students) {
+
+            System.out.println(student1);
+
+        }
+
     }
 
+}
+
+class Student {
+
+    int rollNo;
+    String name;
+
+    @Override
+    public String toString() {
+        return (rollNo + "," + name);
+    }
 }
