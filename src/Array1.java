0c45178390b32cdcc3b3ec2c66e5f5c7471c7a6a
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-13 12:48:14 +0530

    Array concept2 new

--- src/Array1.java
+++ src/Array1.java
@@ -2,15 +2,20 @@ public class Array1 {
 
     public static void main(String[] args) {
 
-        int n = (int)(Math.random() * 100);
+        int n = 5;//(int)(Math.random() * 100);
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = (int)(Math.random() * 100);
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             System.out.println("Array[" +i+ " ] = " + arr[i]);
+            arr[i+1]=arr[i];
         }
+        arr[0] = 17;
         System.out.println(" Number of elements "+ n);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 
 }
