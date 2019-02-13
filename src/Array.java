f2918e58f843ac401ef6189ba4a0424ff460f062
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-13 12:20:03 +0530

    Arrays concept of 2d array

--- src/Array.java
+++ src/Array.java
@@ -30,12 +30,24 @@ public class Array {
         }
 
         int[][] arr3 = new int[20][20];
+        int rowSum = 0;
+        int max = 0;
+        int index = 0;
 
         for (int i = 0; i < 20; i++) {
+            rowSum=0;
             for (int i1 = 0; i1 < 20; i1++) {
                 arr3[i][i1] = (int)(Math.random() * 100);
+                rowSum = rowSum + arr3[i][i1];
             }
+            if( rowSum > max ){
+                max = rowSum;
+                index = i;
+            }
+            System.out.println(" rowSum for " + i + " = " + rowSum);
         }
+
+        System.out.println(" Highest value " + " of rowSum is: " + max + " \nAt index " + index);
 
         for (int[] ints : arr3) {
             for (int ints1 : ints) {
