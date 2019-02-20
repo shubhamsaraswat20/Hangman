f32ebf027af2e88c03ed76596be728cdb7e1df0f
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-20 11:31:46 +0530

    psvm

---
+++ src/MyLinkedList.java
@@ -0,0 +1,47 @@
+public class MyLinkedList {
+
+    private LinkedList head;
+    private LinkedList tail;
+    private int currentSize;
+
+    public LinkedList getHead() {
+        return head;
+    }
+
+    public void setHead(LinkedList head) {
+        this.head = head;
+    }
+
+    public LinkedList getTail() {
+        return tail;
+    }
+
+    public void setTail(LinkedList tail) {
+        this.tail = tail;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
+
+    public MyLinkedList() {
+        this.currentSize = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    public static void main(String[] args) {
+
+        MyLinkedList linkedList = new MyLinkedList();
+
+        LinkedList linkedList1 = new LinkedList(10 , null);
+
+
+
+    }
+
+}
