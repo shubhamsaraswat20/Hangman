621762ea8d99956520c34971b8839e691ecf8fe8
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-20 11:22:32 +0530

    new getter setter constructor

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,7 +1,17 @@
+import javafx.scene.Node;
+
 public class LinkedList {
 
     private int data;
     private LinkedList next;
+
+    public void setNext(LinkedList next) {
+        this.next = next;
+    }
+
+    public LinkedList getNext() {
+        return next;
+    }
 
     public void setData(int data) {
         this.data = data;
@@ -10,6 +20,22 @@ public class LinkedList {
     public int getData() {
         return data;
     }
+
+    public LinkedList(int data, LinkedList next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public LinkedList() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    public LinkedList(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
 
     public static void main(String[] args) {
 
