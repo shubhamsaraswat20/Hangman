4c1477a7c2ffde4cf27d41ddbf7f8e3568282a7f
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-20 11:08:11 +0530

    add getter and seeter

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,6 +2,14 @@ public class LinkedList {
 
     private int data;
 
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
     public static void main(String[] args) {
 
 
