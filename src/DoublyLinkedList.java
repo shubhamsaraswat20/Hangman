c989fead4b9b39bd65c1d154a376fe58182d198f
shubhamsaraswat20 <shubham.saraswat_ccv17@gla.ac.in>
2019-02-27 11:11:10 +0530

    added psvm, insertHead, toString

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,6 +1,6 @@
 public class DoublyLinkedList {
 
-    private static class Node{
+    private static class Node {
 
         private int data;
         private Node pre;
@@ -17,6 +17,53 @@ public class DoublyLinkedList {
             this.pre = null;
             this.next = null;
         }
+
+    }
+
+    private int size = 0;
+    private Node head = null;
+    private Node tail = null;
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        System.out.println(linkedList);
+        for (int i = 0; i < 5; i++) {
+            linkedList.insertHead(i+1);
+        }
+        System.out.println(linkedList);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder response = new StringBuilder();
+
+        response.append("[");
+
+        Node temp = this.head;
+
+        while (temp != null) {
+            response.append(temp.data);
+            if (temp.next != null) {
+                response.append(" <==> ");
+            }
+            temp = temp.next;
+        }
+
+        response.append("]");
+
+        return response.toString();
+
+    }
+
+    private void insertHead(int data) {
+        Node newNode = new Node(data , null , this.head);
+        this.head = newNode;
+        size++;
+    }
+
+    private void insertAfter(int data , Node node) {
 
     }
 
